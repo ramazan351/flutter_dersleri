5a8be3054842f11d8c0e0a751892ef4bbb278249
ramazan351 <31069732+ramazan351@users.noreply.github.com>
2024-02-07 11:58:51 +0300

    data model

---
+++ lib/data_model.dart
@@ -0,0 +1,388 @@
+void main() {
+  Market bMarketi = Market.fromMap(data);
+  print(bMarketi.items?[0].productColor[0].color);
+  // print(bMarketi.items);
+}
+
+class Market {
+  List<MarketProduct>? items = [];
+
+  Market.fromMap(Map<String, List> mapData) {
+    var data = mapData['items'];
+    data?.forEach((element) {
+      var piece = MarketProduct.fromMap(element);
+      items?.add(piece);
+    });
+  }
+}
+
+class MarketProduct {
+  String? type;
+  String? brand;
+  double? price;
+  String? description;
+  List<ProductColor> productColor = [];
+  MarketProduct.fromMap(Map<String, dynamic> dataMap) {
+    type = dataMap['type'];
+    brand = dataMap['brand'];
+    price = double.parse(dataMap['price'].toString());
+    description = dataMap['description'];
+    var colors = dataMap['colors'];
+    (colors as List).forEach((element) {
+      productColor.add(ProductColor.fromMap(element));
+    });
+  }
+}
+
+class ProductColor {
+  String? id;
+  String? color;
+  ProductColor({
+    this.id,
+    this.color,
+  });
+
+  ProductColor.fromMap(Map mapData) {
+    id = mapData['id'];
+    color = mapData['color'];
+  }
+}
+
+Map<String, List> data = {
+  "items": [
+    {
+      "type": "pencil",
+      "brand": "Lamy",
+      "price": 55.5,
+      "description": "Kurşun kalem",
+      "colors": [
+        {
+          "id": "5",
+          "color": "#2F4858",
+        },
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"color": "#198A00"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Graf Von Faber Castell",
+      "price": 550.5,
+      "description": "Kurşun kalem",
+      "colors": [
+        {"id": "8", "color": "#A5378B"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Scrikss",
+      "price": 2000,
+      "description": "Kurşun kalem",
+      "colors": [
+        {
+          "id": "5",
+          "color": "#2F4858",
+        },
+        {"color": "#198A00"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Seed",
+      "price": 20.0,
+      "description": "sill",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Faber Castell",
+      "price": 30.0,
+      "description": "silgi",
+      "colors": [
+        {"id": "2", "color": "#BFFCFA"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Rotting",
+      "price": 85.0,
+      "description": "Kurşun kalem",
+      "colors": [
+        {"id": "2", "color": "#BFFCFA"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Lamy",
+      "price": 55.5,
+      "description": "Kurşun kalem",
+      "colors": [
+        {
+          "id": "5",
+          "color": "#2F4858",
+        },
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"color": "#198A00"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Graf Von Faber Castell",
+      "price": 550.5,
+      "description": "Kurşun kalem",
+      "colors": [
+        {"id": "8", "color": "#A5378B"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Scrikss",
+      "price": 2000,
+      "description": "Kurşun kalem",
+      "colors": [
+        {
+          "id": "5",
+          "color": "#2F4858",
+        },
+        {"color": "#198A00"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Seed",
+      "price": 20.0,
+      "description": "sill",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Faber Castell",
+      "price": 30.0,
+      "description": "silgi",
+      "colors": [
+        {"id": "2", "color": "#BFFCFA"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Seed",
+      "price": 5.0,
+      "description": "sill",
+      "colors": [
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Rotting",
+      "price": 85.0,
+      "description": "Kurşun kalem",
+      "colors": [
+        {"id": "2", "color": "#BFFCFA"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Rotting",
+      "price": 19.9,
+      "description": "sill",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Lamy",
+      "price": 55.5,
+      "description": "Kurşun kalem",
+      "colors": [
+        {
+          "id": "5",
+          "color": "#2F4858",
+        },
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"color": "#198A00"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Lamy",
+      "price": 3.0,
+      "description": "sill",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Graf Von Faber Castell",
+      "price": 550.5,
+      "description": "Kurşun kalem",
+      "colors": [
+        {"id": "8", "color": "#A5378B"},
+      ],
+    },
+    {
+      "type": "pencil",
+      "brand": "Scrikss",
+      "price": 2000,
+      "description": "Kurşun kalem",
+      "colors": [
+        {
+          "id": "5",
+          "color": "#2F4858",
+        },
+        {"color": "#198A00"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Seed",
+      "price": 20.0,
+      "description": "sill",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Faber Castell",
+      "price": 30.0,
+      "description": "silgi",
+      "colors": [
+        {"id": "2", "color": "#BFFCFA"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "duster",
+      "brand": "Rotting",
+      "price": 85.0,
+      "description": "Kurşun kalem",
+      "colors": [
+        {"id": "2", "color": "#BFFCFA"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+    {
+      "type": "joyToy",
+      "brand": "alfa",
+      "price": 20798.0,
+      "description": "",
+      "colors": [
+        {"id": "2", "color": "#ED1C24"},
+        {"id": "8", "color": "#A5378B"},
+        {"id": "0", "color": "#000000"},
+      ],
+    },
+  ]
+};
