cb2f8dcd9a7f08af790daf44704974f0df84f808
ramazan351 <31069732+ramazan351@users.noreply.github.com>
2024-03-04 15:00:52 +0300

    bug fixed

--- lib/homework/main.dart
+++ lib/homework/main.dart
@@ -1,35 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dersi3/homework/list_component.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class KamilHorizontalList extends StatefulWidget {
+  const KamilHorizontalList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
-    );
-  }
+  State<KamilHorizontalList> createState() => _KamilHorizontalListState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class _KamilHorizontalListState extends State<KamilHorizontalList> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
